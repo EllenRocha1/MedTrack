6519d9fd6ecdda17d53e1393b7e065e723607236
EllenRocha1 <ellenthaisy52@gmail.com>
2026-05-17 12:05:44 -0300

    chore: utilizacao da var allowed origin

--- src/main/java/com/medtrack/medtrack/config/WebConfig.java
+++ src/main/java/com/medtrack/medtrack/config/WebConfig.java
@@ -3,15 +3,19 @@ package com.medtrack.medtrack.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002", allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
