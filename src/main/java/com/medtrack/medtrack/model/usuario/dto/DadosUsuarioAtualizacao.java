410d15785ccb157b643c0e2e6fe74b8282280c62
Ellen Rocha <ellenthaisy52@gmail.com>
2026-05-27 02:19:11 -0300

    feat: aplica validação de email/telefone e sanitização de entrada nos DTOs de usuário e dependente

--- src/main/java/com/medtrack/medtrack/model/usuario/dto/DadosUsuarioAtualizacao.java
+++ src/main/java/com/medtrack/medtrack/model/usuario/dto/DadosUsuarioAtualizacao.java
@@ -1,16 +1,29 @@
 package com.medtrack.medtrack.model.usuario.dto;
 
 import com.medtrack.medtrack.model.usuario.CategoriaUsuario;
+import com.medtrack.medtrack.validation.EmailValido;
+import com.medtrack.medtrack.validation.TelefoneValido;
+import com.medtrack.medtrack.validation.ValidationUtils;
 
 import java.time.LocalDate;
 
 public record DadosUsuarioAtualizacao(
         String nome,
+        @EmailValido
         String email,
         String nomeUsuario,
+        @TelefoneValido
         String numeroTelefone,
         String senha,
         CategoriaUsuario tipoConta,
         LocalDate dataNascimento
-) {}
+) {
+    public DadosUsuarioAtualizacao {
+        nome = ValidationUtils.trimIfPresent(nome);
+        email = ValidationUtils.normalizeEmail(email);
+        numeroTelefone = ValidationUtils.normalizeTelefone(numeroTelefone);
+        nomeUsuario = ValidationUtils.trimIfPresent(nomeUsuario);
+        senha = ValidationUtils.trimIfPresent(senha);
+    }
+}
 
